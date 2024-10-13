b7bdc979872e41f776cc4b55593ec51660be683d
Progress with the component <deMiguel@newsaktuell.de>
2024-10-13 17:35:31 +0200

    Refactor WithForm component to include useActionState hook and handle form submission state

--- app/with-form.client.tsx
+++ app/with-form.client.tsx
@@ -2,19 +2,29 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useActionState } from "react";
 
 export default function WithFormClient({
   handleFormData,
 }: {
-  handleFormData: (data: FormData) => void;
+  handleFormData: (
+    prevState: { submitted: boolean },
+    data: FormData
+  ) => Promise<{
+    submitted: boolean;
+  }>;
 }) {
+  const [formState, handleFormDataWrapped] = useActionState(handleFormData, {
+    submitted: false,
+  });
   return (
     <div>
       <h2 className="text-3xl font-semibold">With Form</h2>
-      <form action={handleFormData} className="flex flex-col gap-2">
+      <form action={handleFormDataWrapped} className="flex flex-col gap-2">
         <Input name="name" type="text" placeholder="Lionel Messi" />
         <Button type="submit">Submit</Button>
       </form>
+      <div>{formState.submitted ? "Submitted" : "Not Submitted"}</div>
     </div>
   );
 }
--- app/with-form.server.tsx
+++ app/with-form.server.tsx
@@ -1,10 +1,14 @@
 import WithFormClient from "./with-form.client";
 
 export default function WithForm() {
-  async function handleFormData(data: FormData) {
+  async function handleFormData(
+    prevState: { submitted: boolean },
+    data: FormData
+  ) {
     "use server";
     const values = Object.fromEntries(data.entries());
     console.log(values);
+    return { submitted: true };
   }
 
   return <WithFormClient handleFormData={handleFormData} />;
