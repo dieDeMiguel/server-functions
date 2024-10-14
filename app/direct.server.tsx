c328e5f2cbd7052d34f625f6d356af710ce05041
Progress with the component <deMiguel@newsaktuell.de>
2024-10-14 05:33:43 +0200

    Refactor DirectClient to remove unused addNumbers function

--- app/direct.server.tsx
+++ app/direct.server.tsx
@@ -1,9 +1,5 @@
 import DirectClient from "./direct.client";
 
 export default function Direct() {
-  async function addNumbers(prevState: number, values: number[]) {
-    "use server";
-    return values.reduce((acc, curr) => acc + curr, 0);
-  }
-  return <DirectClient addNumbers={addNumbers} />;
+  return <DirectClient />;
 }
